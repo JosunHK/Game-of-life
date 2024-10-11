2abe7dabb2c289cfd4e6e811379b93cf1d8485b6
JOSUN_HK <dennistai.mail@gmail.com>
2024-10-11 18:34:01 +0800

    Now with RGB

--- app/Main.hs
+++ app/Main.hs
@@ -7,8 +7,13 @@ import Data.List.Split
 import Control.Monad
 import Data.Maybe
 
+data State = State
+    { world :: M.Matrix M.B Bool
+    ,frames :: Int
+    }
+
 size :: Int
-size = 300 
+size = 100 
 
 squareSize :: Float 
 squareSize = 10 
@@ -18,7 +23,6 @@ lifeRules True 2  = True
 lifeRules True 3  = True
 lifeRules False 3 = True
 lifeRules _ _     = False
-
 rolls :: Int -> IO [Bool]
 rolls x = replicateM (x*x) randomIO 
 
@@ -28,8 +32,49 @@ toList = M.fromListsM M.Seq
 populate :: Int -> IO (M.Matrix M.B Bool)
 populate x = do fromJust . toList . chunksOf x <$> rolls x
 
-render' :: M.Matrix M.B Bool -> Picture
-render' = pictures . concat . M.toList . M.imap (\(i :. j) x -> [Translate (fromIntegral i*squareSize - fromIntegral size*squareSize/2) (fromIntegral j*squareSize - fromIntegral size*squareSize/2) $ if x then Color black $ Polygon [(0,0), (0,squareSize), (squareSize,squareSize), (squareSize,0)] else Blank])
+multiplyWorld :: M.Matrix M.B Bool -> M.Matrix M.B Bool
+multiplyWorld = multiplyWorldY . multiplyWorldX 
+    where multiplyWorldX :: M.Matrix M.B Bool -> M.Matrix M.B Bool -- flip and copy the world along the x axis
+          multiplyWorldX x = M.compute $ M.append' 2 x (M.reverse Dim2 x)
+          multiplyWorldY :: M.Matrix M.B Bool -> M.Matrix M.B Bool -- flip and copy the world again along the y axis
+          multiplyWorldY x = M.compute $ M.append' 2 x' (M.reverse Dim2 x')
+            where x' = M.transpose x
+
+
+hueToRGB :: Float -> Float -> Float -> Float
+hueToRGB p q t 
+    | t' < 1/6  = p + (q - p) * 6 * t'
+    | t' < 1/2  = q
+    | t' < 2/3  = p + (q - p) * (2/3 - t') * 6
+    | otherwise = p
+    where t'
+            | t < 0 = t + 1
+            | t > 1 = t - 1
+            | otherwise = t
+
+rainbow :: Float -> Color
+rainbow h = makeColor r g b 1
+        where s = 0.65
+              l = 0.5
+              q = if l < 0.5 then l * (1 + s) else l + s - l * s
+              p = 2 * l - q
+              r = hueToRGB p q (h + 1/3)
+              g = hueToRGB p q h
+              b = hueToRGB p q (h - 1/3)
+
+render :: State -> Picture
+render s = pictures 
+        $ concat 
+        $ M.toList 
+        $ M.imap (\(i :. j) x -> [
+                Translate
+                (fromIntegral i*squareSize - fromIntegral size * squareSize / 2) (fromIntegral j*squareSize - fromIntegral size*squareSize/2)
+                $ if x 
+                  then Color (rainbow (fromIntegral ((i+j+offset) `mod` 360) / 360)) $ Polygon [(0,0), (0,squareSize), (squareSize,squareSize), (squareSize,0)] 
+                  else Blank 
+                ])
+        $ world s
+        where offset = frames s
 
 lifeStencil :: M.Stencil Ix2 Bool Bool
 lifeStencil = M.makeStencil (Sz (3 :. 3)) (1 :. 1) $ \get -> 
@@ -39,10 +84,13 @@ lifeStencil = M.makeStencil (Sz (3 :. 3)) (1 :. 1) $ \get ->
             get ( 1 :. -1), get ( 1 :. 0), get ( 1 :. 1) 
         ]
 
-nextFrame :: a -> b -> M.Matrix M.B Bool -> M.Matrix M.B Bool
-nextFrame _ _ = M.compute . M.mapStencil M.Wrap lifeStencil
+nextFrame :: a -> b -> State -> State
+nextFrame _ _ s = s { world = newWorld, frames = newFrames }
+        where newWorld = M.compute $ M.mapStencil M.Wrap lifeStencil (world s)
+              newFrames = frames s + 1
 
 main :: IO ()
 main = do
-        world <- populate size
-        simulate FullScreen (makeColorI 58 58 58 1) 60 world render' nextFrame
+        world' <- populate size
+        let worldState = State world' 0
+        simulate FullScreen (makeColorI 18 18 18 1) 144 worldState render nextFrame
